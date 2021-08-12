9661215a481649d209323018b73dd7c73ffed1ef
amilcar01 <amilcar.c.salas@gmail.com>
2021-08-12 11:29:49 -0400

    Disenio barra de navegacion

--- index.php
+++ index.php
@@ -1,11 +1,57 @@
-<!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-	
-</body>
-</html>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8"/>
+    <link href='img/logo.png' rel='shortcut icon' type='image/png'/>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <title>SOCABON</title>
+  </head>
+  <body>
+    
+
+    <!---   menu   --->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <img src="img/logo.png" width="40px">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">U.E. Nuetra Sra. de Socabon</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="actividades.php">Actividades</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="plantelDocente.php">Plantel Docente</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="galeriaImg.php">Imagines</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="historia.php">Historia</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Separated link</a>
+              </div>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-sm-2" type="text" placeholder="Buscar">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <!------menu-------->
